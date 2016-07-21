2bda5c5b06a0996dcccf5e6d0b3d5ab10ee670d1
Jack Donaldson <inkybloo@gmail.com>
2016-07-21 11:31:39 +0100

    Old Files and Composer
    This is just the files from version one and the auto-load code from Composer. It does not work as of now.

---
+++ codeV2/app/model/send.php
@@ -0,0 +1,9 @@
+<?php
+
+$mailer = new Mailer();
+
+$mailer->addRecipient('test@example.com');
+$mailer->addRecipient('foo@example.com');
+$mailer->addRecipient('bar@example.com');
+
+$mailer->send('This message has been sent to 3 recipients');
---
+++ codeV2/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+require_once('code/core/autoloader.php');
+spl_autoload_register('Autoloader::loader');
+require_once('code/core/send.php');
